bce39056d2fa026a6d9ed14eb6de5ddd7eb33577
R2035 <r2035.develop@gmail.com>
2021-10-23 14:09:58 +0900

    SearchSectionとSearchItemを実装

---
+++ App/AppStoreReproduction/Screen/F01/SearchItem.swift
@@ -0,0 +1,12 @@
+//
+//  SearchItem.swift
+//  AppStoreReproduction
+//
+//  Created by oe on 2021/10/23.
+//
+
+import Foundation
+
+enum SearchItem {
+    case find(title: String)
+}
---
+++ App/AppStoreReproduction/Screen/F01/SearchSection.swift
@@ -0,0 +1,12 @@
+//
+//  SearchSection.swift
+//  AppStoreReproduction
+//
+//  Created by oe on 2021/10/23.
+//
+
+import Foundation
+
+enum SearchSection {
+    case find
+}
