69b0b5b8493a70eb189a1863462b9b3777083a96
R2035 <r2035.develop@gmail.com>
2021-10-23 11:21:33 +0900

    AppDelegateにcontainerとassemblerを配置

--- App/AppStoreReproduction/App/AppDelegate.swift
+++ App/AppStoreReproduction/App/AppDelegate.swift
@@ -5,15 +5,26 @@
 //  Created by oe on 2021/10/23.
 //
 
+import Swinject
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    private static let container = Container()
+
+    static let assembler: Assembler = {
+        let assembler = Assembler(container: container)
+        assembler.apply(assemblies: [
+            AppAssembly()
+        ])
+        return assembler
+    }()
+
+    // MARK: UIApplicationDelegate
+
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
 
     func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
