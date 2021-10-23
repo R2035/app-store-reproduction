27df56502f1d2aac8885783bd417cf456c9b8ae0
R2035 <r2035.develop@gmail.com>
2021-10-23 14:03:43 +0900

    SearchTextHeaderを実装

---
+++ App/AppStoreReproduction/Screen/F01/SearchTextHeader.swift
@@ -0,0 +1,43 @@
+//
+//  SearchTextHeader.swift
+//  AppStoreReproduction
+//
+//  Created by oe on 2021/10/23.
+//
+
+import Foundation
+import Reusable
+import SnapKit
+import UIKit
+
+final class SearchTextHeader: UITableViewHeaderFooterView, Reusable {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .preferredFont(forTextStyle: .title1)
+        label.textColor = .systemGray
+        label.setContentHuggingPriority(.required, for: .vertical)
+        return label
+    }()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+
+        contentView.backgroundColor = .clear
+
+        contentView.addSubview(titleLabel)
+
+        titleLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 32, left: 16, bottom: 16, right: 16))
+        }
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func update(title: String) {
+        titleLabel.text = title
+    }
+}
