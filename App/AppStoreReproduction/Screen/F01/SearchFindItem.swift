6f7c17d258c6e6d2006930f510f9f72ceab066a2
R2035 <r2035.develop@gmail.com>
2021-10-23 14:07:21 +0900

    SearchFindCellを実装

---
+++ App/AppStoreReproduction/Screen/F01/SearchFindItem.swift
@@ -0,0 +1,42 @@
+//
+//  SearchFindCell.swift
+//  AppStoreReproduction
+//
+//  Created by oe on 2021/10/23.
+//
+
+import Foundation
+import Reusable
+import SnapKit
+import UIKit
+
+final class SearchFindCell: UITableViewCell, Reusable {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemBlue
+        label.numberOfLines = 1
+        label.font = .preferredFont(forTextStyle: .headline)
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        backgroundColor = .systemBackground
+
+        contentView.addSubview(titleLabel)
+
+        titleLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16))
+        }
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func update(title: String) {
+        titleLabel.text = title
+    }
+}
