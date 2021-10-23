7dc479ccd808dfcfb767ae2cdd0100445d95a2ee
R2035 <r2035.develop@gmail.com>
2021-10-23 11:47:45 +0900

    Itemをrename

---
+++ App/AppStoreReproduction/Screen/B01/TodayItem.swift
@@ -0,0 +1,12 @@
+//
+//  TodayItem.swift
+//  AppStoreReproduction
+//
+//  Created by oe on 2021/10/23.
+//
+
+import Foundation
+
+enum TodayItem: Hashable {
+    case feature(description: String, title: String, imageURL: String)
+}
